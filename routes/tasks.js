9b1bc6941cce6fa32bc17ba8b3d417d06da2f652
Dakoda Wogan <d.dakoda@yahoo.com>
2015-05-28 11:53:31 -0500

    adding new tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Task = require('../models/Tasks');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req.body);
+  Task.find(function(error, tasks) {
+    res.json(tasks);
+  });
 });
+
+router.get('/:id', function(req, res, next){
+  Task.findById(req.params.id, function(err, task) {
+    res.json(task);
+  });
+});
+
+//  POST /api/tasks
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  Task.create(req.body, function(err, task) {
+    res.json(task);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  console.log(req.body);
+  Task.findByIdAndUpdate(req.params.id, req.body,
+  function(err, task) {
+    res.json({
+      "message": "Your task has been updated!"
+    });
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  Task.findByIdAndRemove(req.params.id, req.body, function(err, task) {
+    res.send("Your task has been deleted!")
+  });
+});
+
 
 module.exports = router;
